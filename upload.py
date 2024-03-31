b3060047ad0fa81897bd1a7f0c42b47ba4ff1ed9
Zenoor Baig <zenoorbaig@hotmail.co.uk>
2024-03-31 22:42:29 +1000

    test

--- upload.py
+++ upload.py
@@ -1,6 +1,5 @@
-import requests
-import configparser
 from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
@@ -18,16 +17,19 @@ def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file'
-    if file:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File uploaded successfully'
+def upload_files():
+    files = request.files.getlist('files[]')
+    for file in files:
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return 'Files uploaded successfully'
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port =5000)
+    app.run(host='0.0.0.0', debug=True)
